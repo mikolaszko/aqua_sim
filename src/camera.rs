574b86590361f5947752de8b811303c120559121
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-07-02 23:48:01 +0200

    feat: added interface

--- src/camera.rs
+++
@@ -1,9 +0,0 @@
-fn add_people(mut commands: Commands) {
-commands.spawn((
-    Camera3dBundle {
-        transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
-        ..default()
-    },
-    PanOrbitCamera::default(),
-));
-}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 
 use bevy::prelude::* ;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 //.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
 
 fn main() {
     App::new()
+        .init_resource::<UiState>()
         .add_plugins(DefaultPlugins)
         .add_plugins(PanOrbitCameraPlugin)
-        .add_systems(Startup, setup)
+        .add_plugins(EguiPlugin)
+        .add_systems(Startup, setup_sandbox)
+        .add_systems(Update, setup_ui)
         .run();
 }
 
-fn setup(
+fn setup_sandbox(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>
@@ -34,10 +38,20 @@ fn setup(
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
-            intensity: 500_000.0,
+            intensity: 400_000.0,
             ..default()
         },
-        transform: Transform::from_xyz(-3., 2.5, 2.0),
+        transform: Transform::from_xyz(3., 7.5, -2.0),
+        ..default()
+    });
+    //Light 
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            intensity: 400_000.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(-3., 7.5, 2.0),
         ..default()
     });
      // Camera
@@ -49,4 +63,38 @@ fn setup(
         PanOrbitCamera::default(),
     ));
 
+}
+
+#[derive(Default, Resource)]
+struct UiState {
+    label: String,
+    is_dark_mode: bool
+}
+
+fn setup_ui(
+    mut ui_state: ResMut<UiState>,
+    mut contexts: EguiContexts,
+) {
+    let ctx = contexts.ctx_mut();
+
+    egui::SidePanel::left("side_panel")
+        .default_width(200.0)
+        .show(ctx, |ui| {
+            ui.heading("Side Panel");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(&mut ui_state.label);
+            });
+
+
+            ui.checkbox(&mut ui_state.is_dark_mode, "Dark Mode");
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
+                ui.add(egui::Hyperlink::from_label_and_url(
+                    "powered by egui",
+                    "https://github.com/emilk/egui/",
+                ));
+            });
+        });
 }
